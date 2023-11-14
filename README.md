1e2ffcd1d9b15a8b7de6988d6860f21485bd1808
zieraell <roobydemongirl@gmail.com>
2023-11-14 02:13:41 -0500

    Completed Assignment
    - Added Resistor webpage functionality using index.js
    
    - Edited README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Resistor
+# Resistor Calculator
+
+This project deploys a webpage that allows you to calculate the value and tolerance of a resistor based on the color of its bands.
+
+Simply select a color for each of the bands and the value will be calculated automatically!
--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 import { getResistorOhms } from './resistor.js';
+
+const container = document.querySelector('.selection');
+
+const calcBand1 = document.querySelector('#b0');
+const calcBand2 = document.querySelector('#b1');
+const calcMultiplier = document.querySelector('#b2');
+const calcTolerance = document.querySelector('#b3');
+
+const answer = document.querySelector('.answer');
+
+const LOCAL_STORAGE_BANDS_KEY = 'resistor.bands';
+
+let bands = JSON.parse(localStorage.getItem(LOCAL_STORAGE_BANDS_KEY)) || { color1 : "red", color2: "green", multiplier: "violet", tolerance: "gold" };
+
+function saveAndRender() {
+    save();
+    render();
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_BANDS_KEY, JSON.stringify(bands));
+}
+
+function render() {
+    // update band colors
+    calcBand1.className = bands.color1;
+    calcBand2.className = bands.color2;
+    calcMultiplier.className = bands.multiplier;
+    calcTolerance.className = bands.tolerance;
+
+    // update answer
+    answer.textContent = getResistorOhms(bands);
+}
+
+container.addEventListener('click', (e) => {
+    e.preventDefault();
+    // gets click target's color value by splitting it's class string
+    const newColor = e.target.className.split(' ')[1];
+    // gets click target's parent container id using parentNode
+    const newBand = e.target.parentNode.id;
+
+    switch(newBand) {
+        case "color0":
+            bands.color1 = newColor;
+            break;
+        case "color1":
+            bands.color2 = newColor;
+            break;
+        case "color2":
+            bands.multiplier = newColor;
+            break;
+        case "color3":
+            bands.tolerance = newColor;
+            break;
+    }
+    saveAndRender();
+})
+
+render();
